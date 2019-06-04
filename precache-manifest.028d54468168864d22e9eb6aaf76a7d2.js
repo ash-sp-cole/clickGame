22b01b257d487c8c7f5a005bdbb5d12f48dddbe8
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-06-04 16:32:39 +0200

    Updates

---
+++ precache-manifest.028d54468168864d22e9eb6aaf76a7d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524492eef2f7869acc1aab104367362f",
+    "url": "/clickGame/index.html"
+  },
+  {
+    "revision": "8cf4f55b9712c32ae6b6",
+    "url": "/clickGame/static/css/main.e2c3e9f8.chunk.css"
+  },
+  {
+    "revision": "5288cf0e9b721d3a05ff",
+    "url": "/clickGame/static/js/2.cfb8a515.chunk.js"
+  },
+  {
+    "revision": "8cf4f55b9712c32ae6b6",
+    "url": "/clickGame/static/js/main.5598616e.chunk.js"
+  },
+  {
+    "revision": "2cccf4db22d9fda060a7",
+    "url": "/clickGame/static/js/runtime~main.7080151e.js"
+  }
+]);
