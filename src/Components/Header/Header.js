ed9b8ac6b419742aea2969dee813a8f8ba71003d
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-06-04 16:41:37 +0200

    spelling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => (
    <header className="header">
    	<h1>clickGame</h1>
-   	<h2>Click on an image to earn points, but don't click on the smae one Twice!!!</h2>
+   	<h2>Click on an image to earn points, but don't click on the same one Twice!!!</h2>
    </header>
 );
 export default Header;
